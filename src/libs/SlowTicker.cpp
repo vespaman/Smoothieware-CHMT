3653442a6dae4e1cf20880a334bf9e3950384cc9
Matt Baker <matt@mbientlab.com>
2018-10-19 17:33:12 -0700

    stm32: scale down slow tick clock so we can use 16-bit timer

--- src/libs/SlowTicker.cpp
+++ src/libs/SlowTicker.cpp
@@ -21,6 +21,8 @@
 // This module uses a Timer to periodically call hooks
 // Modules register with a function ( callback ) and a frequency, and we then call that function at the given frequency.
 
+#define SLOWTICKER_PRESCALER    (1 << 13)
+
 extern "C" void TIM3_IRQHandler(void);
 
 SlowTicker* global_slow_ticker;
@@ -53,11 +55,12 @@ void SlowTicker::on_module_loaded(){
 
 // Set the base frequency we use for all sub-frequencies
 void SlowTicker::set_frequency( int frequency ){
-    this->interval = (SystemCoreClock >> 2) / frequency;   // SystemCoreClock/4 = Timer increments in a second
+    this->interval = ((SystemCoreClock >> 2) / SLOWTICKER_PRESCALER) / frequency;   // SystemCoreClock/4 = Timer increments in a second
 
+    TIM3->PSC = SLOWTICKER_PRESCALER - 1;
     TIM3->ARR = this->interval;
 
-    flag_1s_count= SystemCoreClock>>2;
+    flag_1s_count= (SystemCoreClock >> 2) / SLOWTICKER_PRESCALER;
 }
 
 // The actual interrupt being called by the timer, this is where work is done
@@ -79,7 +82,7 @@ void SlowTicker::tick(){
     if (flag_1s_count < 0)
     {
         // add a second to our counter
-        flag_1s_count += SystemCoreClock >> 2;
+        flag_1s_count += (SystemCoreClock >> 2) / SLOWTICKER_PRESCALER;
         // and set a flag for idle event to pick up
         flag_1s_flag++;
     }
